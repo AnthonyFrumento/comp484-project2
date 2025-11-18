1e6cfeb279869e13c319f7802fb3488d2c39d88a
Joey <jfrumento15@yahoo.com>
2025-11-17 16:55:20 -0800

    first pet updates

--- script.js
+++ script.js
@@ -7,30 +7,51 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
-  
+    $('.kill-button').click(clickedKillButton);
 
   
     
   })
   
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
-  
+    var pet_info = {
+      name:"My Pet Name", 
+      weight: 10, 
+      happiness: 10,
+      dead: false
+    };
+
     function clickedTreatButton() {
+      if (pet_info.dead) return;
       // Increase pet happiness
+      pet_info.happiness += 1;
       // Increase pet weight
+      pet_info.weight += 1;
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedPlayButton() {
+      if (pet_info.dead) return;
       // Increase pet happiness
+      pet_info.happiness += 1;
       // Decrease pet weight
+      pet_info.weight -= 1;
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedExerciseButton() {
+      if (pet_info.dead) return;
       // Decrease pet happiness
+      pet_info.happiness -= 1;
       // Decrease pet weight
+      pet_info.weight -= 1;
+      checkAndUpdatePetInfoInHtml();
+    }
+
+    function clickedKillButton() {
+      if (pet_info.dead) return;
+      //sets pet to dead
+      pet_info.dead = true;
       checkAndUpdatePetInfoInHtml();
     }
   
@@ -41,6 +62,10 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     
     function checkWeightAndHappinessBeforeUpdating() {
       // Add conditional so if weight is lower than zero.
+      //checks if overweight
+      if (pet_info.weight >= 30) {
+        pet_info.dead = true;
+      }
     }
     
     // Updates your HTML with the current values in your pet_info object
@@ -48,5 +73,19 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       $('.name').text(pet_info['name']);
       $('.weight').text(pet_info['weight']);
       $('.happiness').text(pet_info['happiness']);
+      $('.dead').text(pet_info['dead']);
+
+      //dead pet
+      if (pet_info.dead == true) {
+        //stats change to zero
+        pet_info.name = "DEAD"
+        pet_info.happiness = 0;
+        pet_info.weight = 0;
+        //change image
+        const petImage = document.querySelector(".pet-image");
+        petImage.src = "images/dead.jpg";
+        checkAndUpdatePetInfoInHtml();
+      }
     }
-  
+    
+ 
